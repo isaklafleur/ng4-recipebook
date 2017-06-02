17274107557ddb2a5e329ca4c64ad5642af416d5
Isak La Fleur <isak.engdahl@gmail.com>
2017-06-02 15:54:43 +0200

    updated packages and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './recipe-book/components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/components/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/components/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeListComponent,
-    RecipeItemComponent,
     RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
     HeaderComponent
   ],
   imports: [

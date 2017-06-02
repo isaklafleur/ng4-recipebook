b618ffe8b8ac6d2902c7461e06f5d49ea240a5ee
Isak La Fleur <isak.engdahl@gmail.com>
2017-06-02 17:06:04 +0200

    added some comps.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component'
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
-    HeaderComponent
+    ShoppingEditComponent,
+    HeaderComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Carrot Jummy',
+      'nice carrot pie',
+      'http://http://everydaygoodthinking.com/wp-content/uploads/2015/04/Carrot-Pie-Hamilton-Beach-01.jpg'
+      )
+  ];
 
   constructor() { }
 
